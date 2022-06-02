a760a904ccb36a7b060b827e90f045fbee246742
boilermaker021 <nathan.leakeas@gmail.com>
2022-06-02 01:34:05 -0500

    blank client.c

---
+++ client.c
@@ -0,0 +1,17 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+
+#include "types.h"
+
+
+#define PORT 4130
+
+int main() {
+  
+}
